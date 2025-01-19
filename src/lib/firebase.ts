964d4fecccca85a3f328a29740bf1aa8b662c2c1
PavlosDefoort <psnpenguin2@gmail.com>
2025-01-18 23:38:42 -0500

    Refactor Firebase configuration to use environment variables for sensitive data

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database"; // Import Realtime Database
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcEr8Jqhj1l7i5pZ9UKl7bUJ5MU1cSO6E",
-  authDomain: "angelsadvocate-e77eb.firebaseapp.com",
-  projectId: "angelsadvocate-e77eb",
-  storageBucket: "angelsadvocate-e77eb.firebasestorage.app",
-  messagingSenderId: "439496062633",
-  appId: "1:439496062633:web:1f3f5631586f03dfb5dbb8",
-  measurementId: "G-YWQG5VH6H8",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase app
